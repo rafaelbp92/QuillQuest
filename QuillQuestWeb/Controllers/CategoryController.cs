ab8be40dc777629a0e41604e4abc39967c99fdda
Rafael Pantoja <rafaelbp92@gmail.com>
2024-01-12 21:32:56 +0000

    Adding edit and delete actions

--- QuillQuestWeb/Controllers/CategoryController.cs
+++ QuillQuestWeb/Controllers/CategoryController.cs
@@ -34,5 +34,63 @@ namespace QuillQuestWeb.Controllers
             
             return View();
         }
-    }
+
+		public IActionResult Edit(Guid? id)
+		{
+			if (id == null || id == Guid.Empty)
+			{
+				return NotFound();
+			}
+			Category? category = context.Categories.Find(id);
+			//Category? category = context.Categories.FirstOrDefault(c => c.Id == id);
+			//Category? category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				context.Categories.Update(category);
+				context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
+		public IActionResult Delete(Guid? id)
+		{
+			if (id == null || id == Guid.Empty)
+			{
+				return NotFound();
+			}
+			Category? category = context.Categories.Find(id);
+			//Category? category = context.Categories.FirstOrDefault(c => c.Id == id);
+			//Category? category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(Guid? id)
+		{
+			Category? category = context.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			context.Categories.Remove(category);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
 }
