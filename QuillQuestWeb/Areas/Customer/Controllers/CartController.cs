41c8495d1c3d50bb1320bf8b5ce4c74fa90dfa79
Rafael Pantoja <rafaelbp92@gmail.com>
2024-01-19 22:38:30 +0000

    Adding stripe session creation

--- QuillQuestWeb/Areas/Customer/Controllers/CartController.cs
+++ QuillQuestWeb/Areas/Customer/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using QuillQuest.Utilities;
 using QuillQuest.Models.Enums;
 using QuillQuest.Models.Models;
+using Stripe.Checkout;
 
 namespace QuillQuestWeb.Areas.Customer.Controllers
 {
@@ -13,7 +14,7 @@ namespace QuillQuestWeb.Areas.Customer.Controllers
     {
         [BindProperty]
         public ShoppingCartVM ShoppingCartVm { get; set; }
-		private readonly IUnitOfWork _unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
 
         public CartController(IUnitOfWork unitOfWork)
         {
@@ -26,7 +27,7 @@ namespace QuillQuestWeb.Areas.Customer.Controllers
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
             var carts = _unitOfWork.ShoppingCartRepository.GetAll(c => c.ApplicationUserId == userId, includeProperties: "Product");
 
-			ShoppingCartVm = new()
+            ShoppingCartVm = new()
             {
                 ShoppingCarts = carts,
                 OrderHeader = new()
@@ -35,7 +36,7 @@ namespace QuillQuestWeb.Areas.Customer.Controllers
             foreach (var cart in carts)
             {
                 cart.Price = OrderTotalCalculator.GetPriceBasedOnQuantity(cart);
-				ShoppingCartVm.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+                ShoppingCartVm.OrderHeader.OrderTotal += (cart.Price * cart.Count);
 
             }
             return View(ShoppingCartVm);
@@ -47,25 +48,25 @@ namespace QuillQuestWeb.Areas.Customer.Controllers
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
             var carts = _unitOfWork.ShoppingCartRepository.GetAll(c => c.ApplicationUserId == userId, includeProperties: "Product");
 
-			ShoppingCartVm = new()
+            ShoppingCartVm = new()
             {
                 ShoppingCarts = carts,
                 OrderHeader = new()
             };
 
-			ShoppingCartVm.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUserRepository.Get(u => u.Id == userId);
+            ShoppingCartVm.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUserRepository.Get(u => u.Id == userId);
 
-			ShoppingCartVm.OrderHeader.Name = ShoppingCartVm.OrderHeader.ApplicationUser.Name;
-			ShoppingCartVm.OrderHeader.PhoneNumber = ShoppingCartVm.OrderHeader.ApplicationUser.PhoneNumber;
-			ShoppingCartVm.OrderHeader.StreetAddress = ShoppingCartVm.OrderHeader.ApplicationUser.StreetAddress;
-			ShoppingCartVm.OrderHeader.City = ShoppingCartVm.OrderHeader.ApplicationUser.City;
-			ShoppingCartVm.OrderHeader.State = ShoppingCartVm.OrderHeader.ApplicationUser.State;
-			ShoppingCartVm.OrderHeader.PostalCode = ShoppingCartVm.OrderHeader.ApplicationUser.PostalCode;
+            ShoppingCartVm.OrderHeader.Name = ShoppingCartVm.OrderHeader.ApplicationUser.Name;
+            ShoppingCartVm.OrderHeader.PhoneNumber = ShoppingCartVm.OrderHeader.ApplicationUser.PhoneNumber;
+            ShoppingCartVm.OrderHeader.StreetAddress = ShoppingCartVm.OrderHeader.ApplicationUser.StreetAddress;
+            ShoppingCartVm.OrderHeader.City = ShoppingCartVm.OrderHeader.ApplicationUser.City;
+            ShoppingCartVm.OrderHeader.State = ShoppingCartVm.OrderHeader.ApplicationUser.State;
+            ShoppingCartVm.OrderHeader.PostalCode = ShoppingCartVm.OrderHeader.ApplicationUser.PostalCode;
 
             foreach (var cart in carts)
             {
                 cart.Price = OrderTotalCalculator.GetPriceBasedOnQuantity(cart);
-				ShoppingCartVm.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+                ShoppingCartVm.OrderHeader.OrderTotal += (cart.Price * cart.Count);
 
             }
             return View(ShoppingCartVm);
@@ -73,38 +74,38 @@ namespace QuillQuestWeb.Areas.Customer.Controllers
 
         [HttpPost]
         [ActionName("Summary")]
-		public IActionResult SummaryPOST()
-		{
-			var claimsIdentity = (ClaimsIdentity)User.Identity;
-			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
-			var carts = _unitOfWork.ShoppingCartRepository.GetAll(c => c.ApplicationUserId == userId, includeProperties: "Product");
+        public IActionResult SummaryPOST()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var carts = _unitOfWork.ShoppingCartRepository.GetAll(c => c.ApplicationUserId == userId, includeProperties: "Product");
 
-			ShoppingCartVm.ShoppingCarts = carts;
-			ShoppingCartVm.OrderHeader.OrderDate = DateTime.Now;
+            ShoppingCartVm.ShoppingCarts = carts;
+            ShoppingCartVm.OrderHeader.OrderDate = DateTime.Now;
             ShoppingCartVm.OrderHeader.ApplicationUserId = userId;
 
             ApplicationUser applicationUser = _unitOfWork.ApplicationUserRepository.Get(u => u.Id == userId);
 
             foreach (var cart in carts)
-			{
-				cart.Price = OrderTotalCalculator.GetPriceBasedOnQuantity(cart);
-				ShoppingCartVm.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            {
+                cart.Price = OrderTotalCalculator.GetPriceBasedOnQuantity(cart);
+                ShoppingCartVm.OrderHeader.OrderTotal += (cart.Price * cart.Count);
 
-			}
+            }
 
             if (applicationUser.CompanyId.GetValueOrDefault() == Guid.Empty)
             {
                 // Apply regular customer payment rules
                 ShoppingCartVm.OrderHeader.OrderStatus = OrderStatusEnum.Pending;
-				ShoppingCartVm.OrderHeader.PaymentStatus = PaymentStatusEnum.Pending;
+                ShoppingCartVm.OrderHeader.PaymentStatus = PaymentStatusEnum.Pending;
 
-			}
+            }
             else
             {
-				// Apply company user payment rules
-				ShoppingCartVm.OrderHeader.OrderStatus = OrderStatusEnum.Approved;
-				ShoppingCartVm.OrderHeader.PaymentStatus = PaymentStatusEnum.ApprovedForDelayedPayment;
-			}
+                // Apply company user payment rules
+                ShoppingCartVm.OrderHeader.OrderStatus = OrderStatusEnum.Approved;
+                ShoppingCartVm.OrderHeader.PaymentStatus = PaymentStatusEnum.ApprovedForDelayedPayment;
+            }
 
             _unitOfWork.OrderHeaderRepository.Add(ShoppingCartVm.OrderHeader);
             _unitOfWork.Save();
@@ -124,21 +125,73 @@ namespace QuillQuestWeb.Areas.Customer.Controllers
                 _unitOfWork.Save();
             }
 
-			if (applicationUser.CompanyId.GetValueOrDefault() == Guid.Empty)
-			{
-				// Apply regular customer payment rules
-				// Stripe logic
-			}
+            if (applicationUser.CompanyId.GetValueOrDefault() == Guid.Empty)
+            {
+                // Apply regular customer payment rules
+                // Stripe logic
+                var domain = "https://localhost:7181/";
+                var options = new SessionCreateOptions
+                {
+                    SuccessUrl = $"{domain}/customer/cart/OrderConfirmation?{ShoppingCartVm.OrderHeader.Id}",
+                    CancelUrl = $"{domain}/customer/cart/index",
+                    LineItems = new List<SessionLineItemOptions>(),
+                    Mode = "payment",
+                };
 
-			return RedirectToAction(nameof(OrderConfirmation), new { id=ShoppingCartVm.OrderHeader.Id });
-		}
+                foreach(var item in ShoppingCartVm.ShoppingCarts)
+                {
+                    var sessionLineItems = new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long)(item.Price * 100), // $20.50 => 2050
+                            Currency = "usd",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = item.Product.Title
+                            }
+                        },
+                        Quantity = item.Count
+                    };
+                    options.LineItems.Add(sessionLineItems);
+                }
+                var service = new SessionService();
+                Session session = service.Create(options);
+                _unitOfWork.OrderHeaderRepository.UpdateStripePaymentId(ShoppingCartVm.OrderHeader.Id, session.Id, session.PaymentIntentId);
+                _unitOfWork.Save();
+                Response.Headers.Add("Location", session.Url);
+                return new StatusCodeResult(303);
+            }
+
+            return RedirectToAction(nameof(OrderConfirmation), new { id = ShoppingCartVm.OrderHeader.Id });
+        }
 
         public IActionResult OrderConfirmation(Guid id)
         {
+            OrderHeader orderHeader = _unitOfWork.OrderHeaderRepository.Get(o => o.Id == id, includeProperties: "ApplicationUser");
+
+            if (orderHeader.PaymentStatus != PaymentStatusEnum.ApprovedForDelayedPayment)
+            {
+                // This is an order by customer
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    _unitOfWork.OrderHeaderRepository.UpdateStripePaymentId(id, session.Id, session.PaymentIntentId);
+                    _unitOfWork.OrderHeaderRepository.UpdateStatus(id, OrderStatusEnum.Approved, PaymentStatusEnum.Approved);
+                    _unitOfWork.Save();
+                }
+            }
+
+            List<ShoppingCart>shoppingCarts = _unitOfWork.ShoppingCartRepository.
+                GetAll(c => c.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
+            _unitOfWork.ShoppingCartRepository.RemoveRange(shoppingCarts);
+            _unitOfWork.Save();
+
             return View(id);
         }
 
-		public IActionResult Plus(Guid cartId)
+        public IActionResult Plus(Guid cartId)
         {
             var cartDb = _unitOfWork.ShoppingCartRepository.Get(c => c.Id == cartId);
             cartDb.Count += 1;
