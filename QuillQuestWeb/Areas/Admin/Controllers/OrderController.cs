1d43993d293e6ea2efffd94d04af378df69bc6da
Rafael Pantoja <rafaelbp92@gmail.com>
2024-01-20 10:11:41 +0000

    Adding Order list page

---
+++ QuillQuestWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using QuillQuest.DataAccess.Repository.Interface;
+using QuillQuest.Models.Models;
+using QuillQuest.Utilities;
+
+namespace QuillQuestWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> orderHeaders = _unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+    }
+}
---
+++ QuillQuestWeb/wwwroot/js/order.js
@@ -0,0 +1,28 @@
+
+var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#ordersDataTable').DataTable({
+        "ajax": { url: '/admin/order/getall' },
+        "columns": [
+            { data: 'id', "width": "20%" },
+            { data: 'name', "width": "15%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'applicationUser.email', "width": "15%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'id',
+                render: function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+								<a  href="/admin/order/details?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i></a>
+							</div>`
+                },
+                width: "25%"
+            },
+        ]
+    });
+}
